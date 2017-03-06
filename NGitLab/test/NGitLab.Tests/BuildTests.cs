54f3168eb07aee391836b484d7601d8e9689f7c8
Natalia Langa-Straburzyńska <natalia.langa@castingnetworks.com>
2017-03-06 22:06:13 +0100

    `BuildClients` file name typo corrected; three tests for `BuildClient` added

---
+++ NGitLab/test/NGitLab.Tests/BuildTests.cs
@@ -0,0 +1,54 @@
+﻿using NGitLab.Dotnet.Tests.Fixtures;
+using System;
+using System.Linq;
+using NGitLab.Models;
+using Xunit;
+
+namespace NGitLab.Tests
+{
+    public class BuildTests : IClassFixture<BuildTestFixture>
+    {
+        private readonly IBuildClient _builds;
+
+        public BuildTests(BuildTestFixture buildTestFixture)
+        {
+            _builds = buildTestFixture.RepositoryClient.Builds;
+        }
+
+        [Fact]
+        public async void GetBuilds()
+        {
+            var builds = await _builds.All();
+            Assert.NotNull(builds);
+            Assert.NotEmpty(builds);
+            Assert.Equal(2, builds.Count());
+        }
+
+        [Fact]
+        public async void GetBuildsInScope()
+        {
+            var builds = await _builds.AllInScope(PipelineStatus.success);
+            Assert.NotNull(builds);
+            Assert.NotEmpty(builds);
+            Assert.Equal(1, builds.Count());
+        }
+
+        [Fact]
+        public async void GetBuild()
+        {
+            var build = await _builds["1"];
+            Assert.NotNull(build);
+            Assert.Equal(1, build.Id);
+            Assert.Equal("single_build", build.Name);
+            Assert.Equal("skipped", build.Status);
+            Assert.Equal("build", build.Stage);
+            Assert.Equal("single_build", build.Name);
+            Assert.Equal("ref", build.Ref);
+            Assert.Equal(false, build.Tag);
+            Assert.Equal(null, build.Coverage);
+            Assert.Equal(DateTime.Now.Date, build.CreatedAt);
+            Assert.Equal(null, build.StartedAt);
+            Assert.Equal(null, build.FinishedAt);
+        }
+    }
+}
---
+++ NGitLab/test/NGitLab.Tests/Fixtures/BuildTestFixture.cs
@@ -0,0 +1,122 @@
+﻿using Newtonsoft.Json;
+using NGitLab.Dotnet.Tests.Infrastructure;
+using NGitLab.Models;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using Xunit;
+
+namespace NGitLab.Dotnet.Tests.Fixtures
+{
+    public class BuildTestFixture : BaseFixture
+    {
+        public IGitLabClient Client { get; private set; }
+        public IRepositoryClient RepositoryClient { get; private set; }
+        private const int ProjectId = 1;
+        public User SampleUser { get; set; }
+
+        public BuildTestFixture()
+        {
+            Client = new GitLabClient(ApiUrl, string.Empty, new HttpClient(FakeResponseHandler) { BaseAddress = new Uri(ApiUrl) });
+            RepositoryClient = Client.GetRepository(ProjectId);
+            SampleUser = new User
+            {
+                Id = 1,
+                Name = "Administrator",
+                Email = "admin@example.com",
+                CanCreateGroup = true,
+                Username = "root"
+            };
+        }
+
+        public override void Setup()
+        {
+            FakeResponseHandler.AddFakeGetResponse(
+                new Uri($"{ApiUrl}/api/v3/projects/{ProjectId}/builds"),
+                new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent(
+                        JsonConvert.SerializeObject(
+                            new List<Build>
+                            {
+                                new Build
+                                {
+                                    Id = 1,
+                                    Status = "skipped",
+                                    Stage = "build",
+                                    Name = "sample_build",
+                                    Ref = "ref",
+                                    Tag = false,
+                                    Coverage = null,
+                                    CreatedAt = DateTime.Now.Date,
+                                    StartedAt = null,
+                                    FinishedAt = null,
+                                    User = SampleUser
+                                },
+                                new Build
+                                {
+                                    Id = 2,
+                                    Status = "success",
+                                    Stage = "build",
+                                    Name = "sample_build",
+                                    Ref = "ref",
+                                    Tag = false,
+                                    Coverage = null,
+                                    CreatedAt = DateTime.Now.Date,
+                                    StartedAt = DateTime.Now.Date,
+                                    FinishedAt = DateTime.Now.Date,
+                                    User = SampleUser
+                                }
+                            }))
+                });
+
+            FakeResponseHandler.AddFakeGetResponse(
+                new Uri($"{ApiUrl}/api/v3/projects/{ProjectId}/builds?scope=success"),
+                new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent(
+                        JsonConvert.SerializeObject(
+                            new List<Build>
+                            {
+                                new Build
+                                {
+                                    Id = 2,
+                                    Status = "success",
+                                    Stage = "build",
+                                    Name = "sample_build",
+                                    Ref = "ref",
+                                    Tag = false,
+                                    Coverage = null,
+                                    CreatedAt = DateTime.Now.Date,
+                                    StartedAt = DateTime.Now.Date,
+                                    FinishedAt = DateTime.Now.Date,
+                                    User = SampleUser
+                                }
+                            }))
+                });
+
+            FakeResponseHandler.AddFakeGetResponse(
+                new Uri($"{ApiUrl}/api/v3/projects/{ProjectId}/builds/1"),
+                new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent(
+                        JsonConvert.SerializeObject(
+                            new Build
+                            {
+                                Id = 1,
+                                Status = "skipped",
+                                Stage = "build",
+                                Name = "single_build",
+                                Ref = "ref",
+                                Tag = false,
+                                Coverage = null,
+                                CreatedAt = DateTime.Now.Date,
+                                StartedAt = null,
+                                FinishedAt = null,
+                                User = SampleUser
+                            }))
+                });
+        }
+    }
+}
